f851bad8e6bd78cad48bbf7e22c34e1d4686724a
Manu George <manu@trackplex.com>
2020-05-23 03:14:29 +0530

    Added reusable Input component in shared folder

--- src/places/pages/NewPlace.component.jsx
+++ src/places/pages/NewPlace.component.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Input from '../../shared/components/FormElements/Input.component';
+import './NewPlace.styles.css';
 
 const NewPlace = () =>{
-    return <h2>New Place works!</h2>
+    return <form  className="place-form">
+        <Input 
+        element="input" 
+        type="text" 
+        label="Title" 
+        validators={[]} 
+        errorText="Please Enter a valid Title"/>
+    </form>
 }
 
 export default NewPlace;
