5fe6b519be8bafc9c5b00762bd32dbfcab5bdcba
Manu George <manu@trackplex.com>
2020-06-18 04:02:36 +0530

    Created login& signup page witn authentication using react context & also made few changes in other components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users.component';
 import NewPlace from './places/pages/NewPlace.component';
 import UserPlaces from './places/pages/UserPlaces.component';
 import UpdatePlace from './places/pages/UpdatePlace.component';
+import Auth from './user/pages/Auth.component';
 import MainNavigation from './shared/components/Navigation/MainNavigation.component'
+import { AuthContext } from './shared/context/auth-context';
 const App = () => {
-  return <Router>
-    <MainNavigation />
-    <main>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, [])
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, [])
+  let routes;
+  if (isLoggedIn) {
+    routes = (
       <Switch>
         <Route path="/" exact ><Users /></Route>
         <Route path="/:userId/places" exact><UserPlaces /></Route>
         <Route path="/places/new" exact ><NewPlace /></Route>
-        <Route path="/places/:placeId" exact><UpdatePlace/></Route>
+        <Route path="/places/:placeId" exact><UpdatePlace /></Route>
         <Redirect to="/" />
       </Switch>
-    </main>
-  </Router>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact ><Users /></Route>
+        <Route path="/:userId/places" exact><UserPlaces /></Route>
+        <Route path="/auth"><Auth /></Route>
+        <Redirect to="/auth" />
+      </Switch>
 
+    )
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        login: login,
+        logout: logout
+      }}>
+      <Router>
+        <MainNavigation />
+        <main>
+          {routes}
+        </main>
+      </Router>
+    </AuthContext.Provider>
+  )
 }
 
 export default App;
--- src/places/components/PlaceItem.component.jsx
+++ src/places/components/PlaceItem.component.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Button from '../../shared/components/FormElements/Button.Component';
 import Card from '../../shared/components/UIElements/Card.component';
 import Modal from '../../shared/components/UIElements/Modal.component';
 import Map from '../../shared/components/UIElements/Map.component'
+import { AuthContext } from '../../shared/context/auth-context';
 import './PlaceItem.styles.css';
 
 const PlaceItem = props => {
+    const auth = useContext(AuthContext);
     const [showMap, setShowMap] = useState(false);
     const [showConfirmModal, setShowConfirmModal] = useState(false);
     const openMapHandler = () => setShowMap(true);
@@ -55,8 +57,8 @@ const PlaceItem = props => {
                     </div>
                     <div className="place-item__actions">
                         <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-                        <Button to={`/places/${props.id}`}>EDIT</Button>
-                        <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
+                        {auth.isLoggedIn && (<Button to={`/places/${props.id}`}>EDIT</Button>)}
+                        {auth.isLoggedIn && (<Button danger onClick={showDeleteWarningHandler}>DELETE</Button>)}
                     </div>
                 </Card>
             </li>
