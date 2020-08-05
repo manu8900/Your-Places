8f3ac51116d0d363ef2505b6f0caaa6e60f406e4
Manu George <manu@trackplex.com>
2020-08-05 13:11:47 +0530

    made some changes in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,7 @@ const app = express();
 //app.use is used to register middleware & we use this to register routes of place-routes.js as middleware.
 app.use('/api/places',placesRoutes); ///api/places/- enables us to access routes of places-routes.
 
+//this middleware function express will apply on all request that has an error parameter
 app.use((error,req,res,next)=>{
     if(res.headerSent){//headerSent property checks if any response is already sent.
         return next(error);
@@ -15,7 +16,7 @@ app.use((error,req,res,next)=>{
     res.status(error.code || 500);
     res.json({message:error.message || 'An unknown error occured!'})
 
-}); //this middleware function express will apply on all request that has an error parameter
+}); 
 
 
 app.listen(5000);//port
